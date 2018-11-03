0ee8de8278fea1ffbd5d7be46b86e7de0022b542
Justin Lane <justin@justinlane.org>
2018-11-03 17:20:28 +1300

    Bump Version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule GuardianTrackable.Mixfile do
     [
       app: :guardian_trackable,
       package: package(),
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
